dcf59f2c8882848c16d409a3d6a72b2353259be1
Roman Mukovenkov <roman.mukovenkov@gmail.com>
2026-09-17 11:40:23 +0300

    К определению функции добавлена передаваемая переменная message, которая сразу же потом и возвращается. Добавлены строки документации, чтобы pylint успокоился

--- homework00/hello_world.py
+++ homework00/hello_world.py
@@ -1,2 +1,6 @@
-def text():
+"""Provides a function that simply returns the value it receives as a variable"""
+
+
+def text(message):
+    """Returns the value it receives as a variable"""
     return message
